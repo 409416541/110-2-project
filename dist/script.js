7c52ce906510642f9be377693e126915ffafcfaa
yourname <409416541l@gms.tku.edu.tw>
2022-05-23 21:09:24 +0800

    change microsoft en type setting

--- dist/script.js
+++ dist/script.js
@@ -11,3 +11,19 @@ p.addEventListener('click', function () {
     p.innerHTML = `<i class="fas fa-bars fa-2x"></i>`;
   }
 });
+
+setInterval (play, 3000);
+var index = 0;
+
+function play(){ 
+    var imgs = document.getElementById("thumbs").
+                        getElementsByTagName("img");
+    var bigImg = document.getElementById("big"); 
+    imgs[index].classList.remove("selected");
+    index++; 
+
+    if(index == imgs.length) index = 0;
+
+    imgs[index].classList.add("selected");
+    bigImg.setAttribute("src", imgs[index].getAttribute("src"));
+}
